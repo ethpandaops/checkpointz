a18d48cc20b8a2cb1cec21b3cc235b81b57e6b50
Sam Calder-Mason <sam@puritydev.io>
2022-08-25 16:39:19 +1000

    feat: Download serving bundle in a queue

--- pkg/beacon/metrics.go
+++ pkg/beacon/metrics.go
@@ -7,6 +7,7 @@ import (
 
 type Metrics struct {
 	servingEpoch prometheus.Gauge
+	headEpoch    prometheus.Gauge
 }
 
 func NewMetrics(namespace string) *Metrics {
@@ -16,13 +17,23 @@ func NewMetrics(namespace string) *Metrics {
 			Name:      "serving_epoch",
 			Help:      "The current serving epoch",
 		}),
+		headEpoch: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "head_epoch",
+			Help:      "The current head finalized epoch",
+		}),
 	}
 
 	prometheus.MustRegister(m.servingEpoch)
+	prometheus.MustRegister(m.headEpoch)
 
 	return m
 }
 
 func (m *Metrics) ObserveServingEpoch(epoch phase0.Epoch) {
 	m.servingEpoch.Set(float64(uint64(epoch)))
+}
+
+func (m *Metrics) ObserveHeadEpoch(epoch phase0.Epoch) {
+	m.headEpoch.Set(float64(uint64(epoch)))
 }
