d0cd1df87d9da8549318e9f4de72f9375e008099
Andrew Davis <1709934+Savid@users.noreply.github.com>
2022-08-25 16:44:35 +1000

    feat: checkpointz slot endpoint

--- pkg/service/checkpointz/responses.go
+++ pkg/service/checkpointz/responses.go
@@ -2,10 +2,15 @@ package checkpointz
 
 import (
 	v1 "github.com/attestantio/go-eth2-client/api/v1"
+	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/samcm/checkpointz/pkg/beacon"
 )
 
 type StatusResponse struct {
 	Upstreams map[string]*beacon.UpstreamStatus `json:"upstreams"`
 	Finality  *v1.Finality                      `json:"finality"`
+}
+
+type BeaconSlotResponse struct {
+	Block *spec.VersionedSignedBeaconBlock `json:"block"`
 }
