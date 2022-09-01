6315cd5dfb78b73bffa98e1fcd10f3506af7d237
Sam Calder-Mason <sam@puritydev.io>
2022-09-01 16:19:34 +1000

    feat: Add deposit network id -> network name map

---
+++ pkg/eth/networks.go
@@ -0,0 +1,21 @@
+package eth
+
+func DefaultNetworkIDMap() map[uint64]string {
+	return map[uint64]string{
+		1:        "mainnet",
+		3:        "ropsten",
+		4:        "rinkeby",
+		5:        "goerli",
+		1337802:  "kovan",
+		11155111: "sepolia",
+	}
+}
+
+func GetNetworkName(networkID uint64) string {
+	name, exists := DefaultNetworkIDMap()[networkID]
+	if !exists {
+		return "unknown"
+	}
+
+	return name
+}
