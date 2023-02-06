e9982b0564d54ae531824891a1c497a96079aa17
Sam Calder-Mason <sam@puritydev.io>
2023-02-06 11:49:58 +1000

    feat: Swap samcm/beacon to ethpandaops/beacon

--- pkg/beacon/finality_provider.go
+++ pkg/beacon/finality_provider.go
@@ -6,9 +6,9 @@ import (
 	v1 "github.com/attestantio/go-eth2-client/api/v1"
 	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/ethpandaops/beacon/pkg/beacon/api/types"
+	"github.com/ethpandaops/beacon/pkg/beacon/state"
 	"github.com/ethpandaops/checkpointz/pkg/eth"
-	"github.com/samcm/beacon/api/types"
-	"github.com/samcm/beacon/state"
 )
 
 // FinalityProvider is a provider of finality information.
--- pkg/service/eth/eth.go
+++ pkg/service/eth/eth.go
@@ -7,10 +7,10 @@ import (
 	v1 "github.com/attestantio/go-eth2-client/api/v1"
 	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/ethpandaops/beacon/pkg/beacon/api/types"
+	"github.com/ethpandaops/beacon/pkg/beacon/state"
 	"github.com/ethpandaops/checkpointz/pkg/beacon"
 	"github.com/ethpandaops/checkpointz/pkg/version"
-	"github.com/samcm/beacon/api/types"
-	"github.com/samcm/beacon/state"
 	"github.com/sirupsen/logrus"
 )
 
