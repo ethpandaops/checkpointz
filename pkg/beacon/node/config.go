203e93bfe5b1f63a648e6e0d66daa201ca3f466d
Sam Calder-Mason <sam@puritydev.io>
2023-06-19 10:48:52 +1000

    feat: Update beacon to v0.25.0 and add headers to config

--- pkg/beacon/node/config.go
+++ pkg/beacon/node/config.go
@@ -1,7 +1,8 @@
 package node
 
 type Config struct {
-	Name         string `yaml:"name"`
-	Address      string `yaml:"address"`
-	DataProvider bool   `yaml:"dataProvider"`
+	Name         string            `yaml:"name"`
+	Address      string            `yaml:"address"`
+	DataProvider bool              `yaml:"dataProvider"`
+	Headers      map[string]string `yaml:"headers"`
 }
--- pkg/beacon/nodes.go
+++ pkg/beacon/nodes.go
@@ -24,8 +24,9 @@ func NewNodesFromConfig(log logrus.FieldLogger, configs []node.Config, namespace
 
 	for i, config := range configs {
 		sconfig := &sbeacon.Config{
-			Name: config.Name,
-			Addr: config.Address,
+			Name:    config.Name,
+			Addr:    config.Address,
+			Headers: config.Headers,
 		}
 
 		opts := *sbeacon.DefaultOptions()
